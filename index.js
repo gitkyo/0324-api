8105bd90f724cc668a0922c75228208899147da2
Pier <p.delaunay1@gmail.com>
2023-09-27 16:38:38 +0400

    add comment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+//console.log permet d'afficher des message dans le terminal (cote backend)
 console.log("coucou ceci est un test de connexion à mysql")
 
-//connexion avec mysql
+//cette ligne me permet d'importer le module mysql : https://www.npmjs.com/package/mysql
 import mysql from 'mysql'
-//Connection to MySQL database    
+
+//Connection to MySQL database 
+// ici on créer une vairbale nommé db qui va contenir notre acces à la BDD
 const db = mysql.createConnection({
   user: 'root',
   password: '',
@@ -10,25 +13,48 @@ const db = mysql.createConnection({
   database: '0324',
 });
 
-// test connexion
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Connected to database');
-});
+//ici on créer un try catch pour gérer les erreurs
+try{   
 
-//get Task data
-const getAlltask = () => {
-     //on ajoute les données dans la base de données sans ORM
-    db.query("SELECT * FROM task ", (error, result) => {
-    if (error) {
-        console.log(error)
-    }    
-        console.log(result)
-    }); 
+    // ici j'appelle la fonction db pour me connecter à la base de donnée
+    db.connect((err) => {
+
+        //si j'ai une erreur je l'affiche dans le terminal
+        if (err) {
+            throw err;
+        }
+
+        //sinon tout va bien je suis connecté
+        console.log('Connected to database');
+    });
+
+//si une erreur est attrapé je l'affiche dans le terminal
+}catch(error){
+    console.log(error)
 }
 
+
+
+//get Task data
+const getAlltask = () => {     
+
+    try{
+        //on utilise la varibale db pour effectuer une requete SQL qui récupére toutes les taches
+        db.query("SELECT * FROM task ", (error, result) => {       
+        
+            if (error) {
+            throw error
+        }    
+            console.log(result)
+        }); 
+
+    }catch(error){
+        console.log(error)
+    }
+
+   
+}
+//ici une fonction nommé getTaskFromIdUser qui prend en parametre un id
 const getTaskFromIdUser = (id) => {
     db.query("SELECT * FROM task WHERE owner = ?", [id], (error, result) => {
         if (error) {
@@ -47,6 +73,7 @@ const getTaskFromNameUser = (name) => {
         }); 
 }
 // getAlltask()
+//ici j'appelle la fonction getTaskFromIdUser avec en parametre 1
 // getTaskFromIdUser(1)
 // getTaskFromNameUser('toto')
 
