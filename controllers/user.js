5b9f6599f61b9c60a7b2e91b7c79149fc8b1f783
Pier <p.delaunay1@gmail.com>
2023-10-10 16:35:56 +0400

    add getUserById  and getAllUser  controller / routeur

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,29 @@ export const getAllUser = async (req, res) => {
         res.status(200).send(users);
 
     } catch (error) {        
-        console.log(error)
+        res.send(error) 
+    }
+}
+
+export const getUserById = async (req, res) => {
+    try {
+        //on récupére l'id de la tache dans l'url
+        const id = req.params.id        
+
+        //get Task By Id  with the orm sequelize and find with where clause
+        const user = await User.findAll({
+            where: {
+                id: id
+            }
+        }); 
+
+        if(!user) {
+            res.status(404).send('no users found')
+        }
+
+        res.status(200).send(user);
+    } catch (error) {
+        res.send(error)
     }
 }
 
--- router/user.js
+++ router/user.js
@@ -5,9 +5,14 @@ import express from "express";
 export const userRouter = express.Router();
 
 //import controller
-import { getAllUser } from '../controllers/user.js'
+import { getAllUser, getUserById } from '../controllers/user.js'
 
 //route get sur l'url /tasks pour obtenir toutes les taches
 userRouter.get('/users', (req, res) => {
     getAllUser(req, res);
+})
+
+//route get sur l'url /tasks/id pour obtenir une tache en fonction de son id
+userRouter.get('/users/:id', (req, res) => {
+    getUserById(req, res);
 })
